f7052e8a5a06e28055c9deae70aaa77c74dc50ae
LukasSchmoelzl <lukas@schmoelzl.de>
2025-10-12 20:57:53 +0200

    docs: Add comment explaining nodeTypes is module-level constant - React Flow nodeTypes warning is expected in HMR dev-mode - Nodes are already memoized individually - Build successful

--- src/canvas/nodes/nodes/index.ts
+++ src/canvas/nodes/nodes/index.ts
@@ -3,6 +3,8 @@ import { IfcNode } from "./ifc-node"
 import { TemplateNode } from "./template-node"
 import { InfoNode } from "./info-node"
 
+// Node types are already memoized in their respective files
+// This object is created once at module load time
 export const nodeTypes: NodeTypes = {
   ifcNode: IfcNode,
   templateNode: TemplateNode,
